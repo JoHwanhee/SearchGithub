23fe6cde929a20b276dcaf8b35e955e635c80bea
johwanhui <kikiki0611@gmail.com>
2022-04-05 23:56:22 +0900

    feat: add kotest dependency
    close: #9

--- app/src/test/java/com/hwanhee/search_github/ExampleUnitTest.kt
+++ app/src/test/java/com/hwanhee/search_github/ExampleUnitTest.kt
@@ -1,17 +1,18 @@
 package com.hwanhee.search_github
 
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+class MyTests : FunSpec(
+    {
+        test("샘플 스트링 테스트")
+        {
+            "sample".length shouldBe 6
+            "".length shouldBe 0
+        }
     }
-}
+)
+
